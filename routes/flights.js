8b6e2997044cba22f5770696e96b3cd5db2d7563
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-20 12:36:11 -0400

    implement index flight page

--- routes/flights.js
+++ routes/flights.js
@@ -4,10 +4,12 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 /* GET users listing. */
+router.get('/',flightsCtrl.index)
+
 router.get('/new', flightsCtrl.new)
 
 router.post('/', flightsCtrl.create)
 
 export {
-  router
+  router,
 }
